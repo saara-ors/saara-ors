eeea31829b24bd3ba9b9b3c62ae50c3f157c3ea5
saara-ors <souzasarahferreirasouza@gmail.com>
2024-07-30 12:23:51 -0300

    oii

---
+++ main.js
@@ -0,0 +1,80 @@
+</section>
+<script src="main.js"></script>
+</body>
+</html>
+const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao");
+console.log(botoes);
+const botoes = document.querySelectorAll(".botao");
+
+for()
+    const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;)
+    const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    console.log();
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick
+    console.log();
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        botoes[i].classList.add("ativo");
+    }
+    console.log();
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+    console.log();
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+}
+<script src="main.js"></script>
+const botoes = document.querySelectorAll(".botao");
+console.log(botoes)
+VVfor ([inicialização]; [condição]; [incremento]) {
+    declaração
+    }
+    for (var i = 1; i <= 5; i++) {
+        console.log(i);
+      }
+      for (var i = 0; i <= 10; i += 2) {
+        console.log(i);
+      }
+      var frutas = ["Maçã", "Banana", "Laranja", "Pera"];
+      for (var i = 0; i < frutas.length; i++) {
+        console.log(frutas[i]);
+      }
+      let contador = 5;
+      contador += 3;
+      let contador = 10;
+      contador -= 4;
+      var numero = 3;
+      numero *= 2;
+      var numero = 10;
+      numero /= 2; 
+                                           
